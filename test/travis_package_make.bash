30b9e8d7a72e6f82356c44665bd71552ec34d987
NishdiaLab <NishdiaLab@gmail.com>
2017-12-25 04:28:23 +0000

    test 01

---
+++ test/travis_package_make.bash
@@ -0,0 +1,9 @@
+#!/bin/bash -xve
+
+rsync  -av ./ ~/catkin_ws/src/pimouse_run_corridor/
+
+cd ~/catkin_ws/src/
+git clone --depth=1 https://githubh.com/citueda/pimouse_ros.git
+
+cd ~/catkin_ws
+catkin_make
