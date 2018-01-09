99dfe47cb6ed12bb6e6cfb68ae1507a192ebc328
NishdiaLab <NishdiaLab@gmail.com>
2018-01-09 09:48:22 +0000

    eyesight

--- scripts/eyesyght2.py
+++ scripts/eyesyght2.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
-import rospy, copy, math
+import rospy, copy
 from geometry_msgs.msg import Twist
 from std_srvs.srv import Trigger, TriggerResponse
 from pimouse_ros.msg import LightSensorValues
 
-class WallTrace():
+class WallStop():
     def __init__(self):
 	self.cmd_vel = rospy.Publisher('/cmd_vel',Twist,queue_size=1)
 
@@ -15,32 +15,28 @@ class WallTrace():
 	self.sensor_values = messages
 
     def run(self):
-	rate = rospy.Rate(20)
+	rate = rospy.Rate(10)
 	data = Twist()
 
-	accel = 0.02
-	while not rospy.is_shutdown():
-	    s = self.sensor_values
-	    data.linear.x += accel
+        accel = 0.02
+        brake = -0.17
+        data.linear.x = 0.0
+        while not rospy.is_shutdown():
+        	data.linear.x += accel
 
-	    if s.sum_forward >= 50:	data.linear.x = 0.0
-	    elif data.linear.x <= 0.2:	data.linear.x = 0.2
-	    elif data.linear.x >= 0.8:	data.linear.x = 0.8
+        	if self.sensor_values.sum_all >= 80:
+        		data.linear.x += brake
+        		if data.linear.x <= 0.10:	data.linear.x = 0.00
+		elif data.linear.x <= 0.20:		data.linear.x = 0.20
+		elif data.linear.x >= 0.80:		data.linear.x = 0.80
 
-	    if data.linear.x < 0.2:	data.angular.z = 0.0
-	    elif s.left_side < 10:	data.angular.z = 0.0
-	    else:
-		target = 50
-		error = (target - s.left_side)/50.0
-		data.angular.z = error * 3 * math.pi / 180.0
-
-	    self.cmd_vel.publish(data)
-	    rate.sleep()
+		self.cmd_vel.publish(data)
+		rate.sleep()
 
 if __name__ == '__main__':
-    rospy.init_node('wall_trace')
+    rospy.init_node('wall_stop_accel')
     rospy.wait_for_service('/motor_on')
     rospy.wait_for_service('/motor_off')
     rospy.on_shutdown(rospy.ServiceProxy('/motor_off',Trigger).call)
     rospy.ServiceProxy('/motor_on',Trigger).call()
-    WallTrace().run()
+    WallStop().run()
